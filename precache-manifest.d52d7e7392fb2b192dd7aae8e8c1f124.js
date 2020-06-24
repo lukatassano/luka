014eed00ab6cd98d835414ff752d28378383de2e
Luka Tassano <lukatassano070@gmail.com>
2020-06-24 03:48:45 -0300

    Updates

---
+++ precache-manifest.d52d7e7392fb2b192dd7aae8e8c1f124.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10e3959451c612163307b1ec05751bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc42ac8ab91903e86536",
+    "url": "/static/css/main.3b1d65ee.chunk.css"
+  },
+  {
+    "revision": "29996ba3ab7a6a955ef5",
+    "url": "/static/js/2.7e898fad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7e898fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc42ac8ab91903e86536",
+    "url": "/static/js/main.ec0380db.chunk.js"
+  },
+  {
+    "revision": "01fab395909b3cadb305",
+    "url": "/static/js/runtime-main.cb3ce9c0.js"
+  },
+  {
+    "revision": "159fef478be0dec2df7e5fa244a78415",
+    "url": "/static/media/curriculo.159fef47.pdf"
+  },
+  {
+    "revision": "8c141d7c2b51dbdee4e59246642ca5ea",
+    "url": "/static/media/logo.8c141d7c.png"
+  }
+]);
