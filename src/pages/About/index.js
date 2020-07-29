79d07a113322b017e0eea104530e88ea2dfffbb3
Luka Tassano <lukatassano070@gmail.com>
2020-07-29 10:02:38 -0300

    change text about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -10,12 +10,25 @@ function About() {
         <h1>SOBRE</h1>
         <div className="text">
           <p>
-            Desenvolvedor full stack Javascript. Experiência em
-            Node.js(back-end), React(front-end) e React Native(mobile).
+            Apaixonado por programação e suas possibilidades de automatizar e
+            facilitar processos, com ela consegui fazer:
+            <br />
+            <br />- Um pequeno assistente virtual para Windows
+            <br />- Detector de gás e fumaça wifi
+            <br />- Irrigador de plantas automatizado por horário com sensor de
+            umidade controlado por aplicativo
+            <br />
+            <br />
+            Nessa trajetória conheci linguagens como C, C#, Python, Java e
+            Javascript com HTML e CSS onde estou me especializando hoje,
+            estudando frameworks como Node.js, React e React Native para dominar
+            o back-end, front-end e mobile. Sempre disposto a aprender algo
+            novo, mais eficiente e/ou benéfico para o projeto. Flexível,
+            comunicativo e sempre com muito foco no resultado
           </p>
         </div>
 
-        <a href="#work">
+        <a href="#projects">
           <div className="btn btn-one">
             <IoIosArrowDown className="img" color="#fff" size={50} />
           </div>
